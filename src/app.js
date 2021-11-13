d8e71a2c19db3c48c0d3f54b443dbe48fa874013
Igor <gitihonovich@gmail.com>
2021-11-13 18:57:04 +0300

    feat(input): Add configuration form input and submit button.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+import { isValid } from './utils';
 import './stule.css'
 
-console.log('app')
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitButton = form.querySelector('#submit-button')
+
+form.addEventListener('submit', submitFormHandler)
+
+function submitFormHandler(event) {
+  event.preventDefault()
+
+  if(isValid(input.value)) {
+
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON(),
+    }
+
+    submitButton.disabled = true
+
+    // Async request to server to save question
+    console.log('question', question)
+    input.value = ''
+    input.className = ''
+    submitButton.disabled = false
+  }
+}
